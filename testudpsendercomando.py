a8f2fb1220120f351543e178844b8a96cf6d9cf0
Santiago <santiacquarone@gmail.com>
2020-10-19 17:19:15 -0300

    test con parametro por comando

---
+++ testudpsendercomando.py
@@ -0,0 +1,14 @@
+import socket
+import sys
+
+UDP_IP = "192.168.1.46"
+UDP_PORT = 2424
+MESSAGE = sys.argv[0].encode()
+
+print("UDP target IP: %s" % UDP_IP)
+print("UDP target port: %s" % UDP_PORT)
+print("message: %s" % MESSAGE)
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
